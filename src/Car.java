e2f27280ebd06e2e25a05c3b2fd4a89c28be2eba
gamalchev19 <GAMalchev19@codingburgas.bg>
2022-10-19 13:11:47 +0300

    Update

---
+++ src/Car.java
@@ -0,0 +1,13 @@
+
+    public class Car {
+        String brand;
+        String model;
+        void start( String brand, String model )
+
+        {
+
+        }
+    }
+
+
+
---
+++ src/Lamp.java
@@ -0,0 +1,12 @@
+public class Lamp {
+    String name;
+    void setLampName(String name)
+    {
+        this.name=name;
+    }
+    void getLampName()
+    {
+        System.out.println(this.name);
+    }
+
+}
